1f95df5e3fc921210e58761874161ad25b84d6b1
Clarks1223 <pablo.uchuari@epn.edu.ec>
2023-02-13 11:29:24 -0500

    Diseño de login 80%

--- src/Login.java
+++ src/Login.java
@@ -1,7 +1,28 @@
 import javax.swing.*;
 
-public class Login {
+public class Login extends ventanas{
     private JPanel FLogin;
     private JLabel LNomEmpresa;
     private JLabel LBienvenido;
+    private JTextField textField1;
+    private JLabel JLRoldeUsuario;
+    private JLabel JLingreseUsuario;
+    private JPasswordField passwordField1;
+    private JLabel JLingreseContrasenia;
+    private JComboBox CBRoldeUsuario;
+    private JLabel JLImagenBuho;
+    static JFrame frame=new JFrame("Login");
+    @Override
+    public void abrirVentana(){
+        frame.setContentPane(new Login().FLogin);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
+    @Override
+    public void cerrarVentana(){
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        Login ventana1= new Login();
+        ventana1.abrirVentana();
     }
 }
