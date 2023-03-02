ad36fd6441dee502acfe42798e506cfa5759de11
Erick <erick.santillan@epn.edu.ec>
2023-03-02 13:31:10 -0500

    Anadir funcionalidades a la pantalla cajero.

--- src/Cajero.java
+++ src/Cajero.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.*;
 
 public class Cajero extends ventanas{
     private JPanel PanelCajero;
@@ -13,11 +15,27 @@ public class Cajero extends ventanas{
     private JLabel JLgetNombre;
     private JLabel JLgetID;
     private JLabel JLdescripcionProd;
-    private JLabel JLgetDescripoPro;
+    private JLabel JTsetDescripoPro;
+    private JLabel JTsetPrecio;
+    private JLabel JTsetUnidad;
+    PreparedStatement pd;
 
     static JFrame addcajero =  new JFrame("Cajero");
 
     public Cajero() {
+        Connection cn;
+        ConxBD con = new ConxBD();
+        try {
+            cn = con.estbConexion();
+            Statement consultaPro = cn.createStatement();
+            ResultSet resultadoPro = consultaPro.executeQuery("Select * from anioD");
+            while (resultadoPro.next()) {
+                comboBox1.addItem(resultadoPro.getString(1));
+            }
+            cn.close();
+        } catch (HeadlessException | SQLException f) {
+            System.err.println(f);
+        }
         buttonRegistrar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -25,6 +43,58 @@ public class Cajero extends ventanas{
                 Venta ventLogin = new Venta();
                 ventLogin.abrirVentana();
                 addcajero.setVisible(false);
+            }
+        });
+        CERRARButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Login loginCaj = new Login();
+                loginCaj.abrirVentana();
+                addcajero.setVisible(false);
+            }
+        });
+        buttonRegistrar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Connection cn;
+                ConxBD con = new ConxBD();
+                try {
+                    cn = con.estbConexion();
+                    pd = cn.prepareStatement("Insert into datosVehiculo VALUES (?,?,?,?,?,?)");
+                    pd.setString(1, TFsetUnidades.getText());
+                    System.out.println(pd);
+                    int res = pd.executeUpdate();
+                    if (res > 0) {
+                        JOptionPane.showMessageDialog(null,"Producto guardado con exito");
+                    } else {
+                        JOptionPane.showMessageDialog(null,"Error");
+                    }
+                    cn.close();
+                } catch (Exception ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+        comboBox1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Connection cx;
+                ConxBD con = new ConxBD();
+                try{
+                    cx = con.estbConexion();
+                    String qr = "select * from datosVehiculo  where matricula = "+ comboBox1.getAction()+";";
+                    Statement s = cx.createStatement();
+                    ResultSet rs = s.executeQuery(qr);
+                    System.out.println(rs);
+                    while(rs.next()) {
+                        JLdescripcionProd.setText(rs.getString(2));
+                        JTsetPrecio.setText(rs.getString(3));
+                        JTsetUnidad.setText(rs.getString(4));
+                    }
+                    cx.close();
+                } catch (Exception ex) {
+                    throw new RuntimeException(ex);
+                }
             }
         });
     }
@@ -40,5 +110,14 @@ public class Cajero extends ventanas{
     @Override
     public void cerrarVentana(){
         addcajero.setVisible(false);
+    }
+    public static void main(String[] args) {
+        JFrame fr = new JFrame("Cajero");
+        fr.setContentPane(new Cajero().PanelCajero);
+        fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fr.pack();
+        fr.setVisible(true);
+        fr.setResizable(false);
+        fr.setLocationRelativeTo(null);
     }
 }
