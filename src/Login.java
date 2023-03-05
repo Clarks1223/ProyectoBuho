ba9141ba25d1adf005caabfae96a308c038a4cd9
Clarks1223 <pablo.uchuari@epn.edu.ec>
2023-03-05 01:37:39 -0500

    Cargando Login

--- src/Login.java
+++ src/Login.java
@@ -1,8 +1,13 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Login extends ventanas{
     private JPanel FLogin;
@@ -17,17 +22,40 @@ public class Login extends ventanas{
     private JLabel JLImagenBuho;
     private JButton JBiniciarsecion;
     static JFrame frame=new JFrame("Login");
+
+    ConxBD conectar = new ConxBD();
+    Connection con;
     String tipousuario = "";
+    String usuario="";
+
     public Login() {
         JBiniciarsecion.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 tipousuario=JCBroldeUsuario.getSelectedItem().toString();
+                usuario = JTFusuario.getText();
                 if (tipousuario.equals("Administrador")){
+                    try{
+                        con = conectar.estbConexion();
+                        Statement consultaUsuario = con.createStatement();
+                        ResultSet resultadoUsuario = consultaUsuario.executeQuery("select * from login_administrador where usuAdmin="+usuario
+                        +" and ");
+                        con.close();
+                    }catch(HeadlessException | SQLException f){
+                        System.err.println(f);
+                    }
                     AdminPag1 inicioAdmin = new AdminPag1();
                     inicioAdmin.abrirVentana();
                     cerrarVentana();
                 }else{
+                    try {
+                        con=conectar.estbConexion();
+                        Statement consultaUsuario = con.createStatement();
+                        ResultSet resultadoUsuario = consultaUsuario.executeQuery("select * from login_cajero where usuCaj="+usuario);
+                        con.close();
+                    }catch (HeadlessException | SQLException f){
+                        System.err.println(f);
+                    }
                     Cajero inicioCajero = new Cajero();
                     inicioCajero.abrirVentana();
                     cerrarVentana();
