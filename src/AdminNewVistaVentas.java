508de6e99a35a64618b77b28eb8be3474cd67009
Clarks1223 <pablo.uchuari@epn.edu.ec>
2023-03-05 16:33:41 -0500

    Cargar cajeros en el combobox para realizar las busquedas de sus ventas

--- src/AdminNewVistaVentas.java
+++ src/AdminNewVistaVentas.java
@@ -1,7 +1,12 @@
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class AdminNewVistaVentas extends ventanas{
     private JPanel JPadmpag3;
@@ -16,10 +21,28 @@ public class AdminNewVistaVentas extends ventanas{
     private JTable JTdatoscajero;
     private JLabel JLtitulo2;
     private JTable table1;
+    //objeto para el frame
     static JFrame busqueda = new JFrame();
+    //objetos para la coneccion
+    ConxBD conectarBD = new ConxBD();
+    Connection con;
     DefaultTableModel dtm = new DefaultTableModel();
     String[] titulo = new String[]{"ID","Nombre","Apellido"};
 public AdminNewVistaVentas(){
+    //Seteo resumen total como predefinido
+    JRBresumen.setSelected(true);
+    //Cargo los datos de los cajeros
+    try{
+        con = conectarBD.estbConexion();
+        Statement consultaCajeros = con.createStatement();
+        ResultSet resultadoCajero = consultaCajeros.executeQuery("select cedCaj from datos_cajero");
+        while (resultadoCajero.next()){
+            JCBcajeros.addItem(resultadoCajero.getString(1));
+        }
+        con.close();
+    }catch (HeadlessException | SQLException f){
+        System.err.println(f);
+    }
     JBcerrarsesion.addActionListener(new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
