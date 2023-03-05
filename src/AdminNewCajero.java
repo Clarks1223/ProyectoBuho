f75773694fdf10b60b4fed253c75ab35a2fcab82
Clarks1223 <pablo.uchuari@epn.edu.ec>
2023-03-05 15:58:50 -0500

    Cajeros y productos conectados con la base de datos

--- src/AdminNewCajero.java
+++ src/AdminNewCajero.java
@@ -1,6 +1,10 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class AdminNewCajero extends ventanas{
     private JPanel JPadmpag4;
@@ -19,8 +23,13 @@ public class AdminNewCajero extends ventanas{
     private JLabel JLdireccion;
     private JLabel JLcelular;
     private JTextField JTFcelular;
+    private JTextField JTFcontrasenia;
+    private JLabel JLcontrasenia;
     //Variables globales
     static JFrame addcajero =  new JFrame("Nuevo Empleado");
+    //Objeto para la contrasenia
+    ConxBD conectarBD = new ConxBD();
+    Connection con;
 
     public AdminNewCajero() {
         JBcerrarsesion.addActionListener(new ActionListener() {
@@ -37,6 +46,46 @@ public class AdminNewCajero extends ventanas{
                 AdminPag1 back = new AdminPag1();
                 back.abrirVentana();
                 cerrarVentana();
+            }
+        });
+        JBagregarempleado.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int resultado=0;
+                int resultado1=0;
+                try {
+                    PreparedStatement ps;
+                    PreparedStatement ps1;
+                    con=conectarBD.estbConexion();
+                    ps = con.prepareStatement("Insert into datos_cajero values (?,?,?,?)");
+                    ps.setString(1, JTFcedula.getText());
+                    ps.setString(2, JTFnombre.getText());
+                    ps.setString(3, JTFdireccion.getText());
+                    ps.setString(4, JTFcelular.getText());
+                    resultado=ps.executeUpdate();
+                    ps1 = con.prepareStatement("Insert into login_cajero values (?,?)");
+                    ps1.setString(1, JTFcedula.getText());
+                    ps1.setString(2, JTFcontrasenia.getText());
+                    resultado1=ps1.executeUpdate();
+                    if (resultado>0 && resultado1>0){
+                        JOptionPane.showMessageDialog(null,"Cajero agregado");
+                    }else {
+                        JOptionPane.showMessageDialog(null,"Cajero no agregado, Ya ha ingresado el numero de ceula");
+                    }
+                    con.close();
+                }catch (HeadlessException | SQLException f){
+                    System.err.println(f);
+                }
+            }
+        });
+        JBborracampos.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JTFcedula.setText("");
+                JTFnombre.setText("");
+                JTFdireccion.setText("");
+                JTFcelular.setText("");
+                JTFcontrasenia.setText("");
             }
         });
     }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         //Abrir la ventana del programa
-        Login ventana1= new Login();
-        ventana1.abrirVentana();
+        //Login ventana1= new Login();
+        //ventana1.abrirVentana();
+        AdminNewCajero vent1 =  new AdminNewCajero();
+        vent1.abrirVentana();
     }
 }
