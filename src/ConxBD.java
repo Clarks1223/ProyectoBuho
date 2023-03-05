f9647259267ca8f452c48e60f8e30f3c933ad1a1
Clarks1223 <pablo.uchuari@epn.edu.ec>
2023-03-05 15:13:58 -0500

    Login Done!!!

--- src/ConxBD.java
+++ src/ConxBD.java
@@ -3,10 +3,10 @@ public class ConxBD {
     public Connection estbConexion() {
         Connection con = null;
         /*Datos para la coneccion */
-        String base="ProyectoBuho";
+        String base="";
         String url= "jdbc:mysql://localhost:3306/"+base;
         String user="root";
-        String password="3264";
+        String password="";
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             con=DriverManager.getConnection(url,user,password);
