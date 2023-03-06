be76a8e117096de4f387d689c53c824c97d7408c
Erick <erick.santillan@epn.edu.ec>
2023-03-06 01:15:03 -0500

    Actualizacion final de pantalla cajero y venta

--- src/Cajero.java
+++ src/Cajero.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
@@ -13,23 +14,23 @@ public class Cajero extends ventanas{
     private JLabel JLnombre;
     private JLabel JLgetNombre;
     private JLabel JLgetID;
-    private JLabel JLdescripcionProd;
-    private JLabel JTGetDescripoPro;
+    private JLabel JLnombreProd;
+    private JLabel JTGetNombrePro;
     private JLabel JTgetPrecio;
     private JLabel JTgetUnidad;
     private JLabel JLtitulo2;
+    private JButton BUSCARButton;
     PreparedStatement pd;
 
     static JFrame addcajero =  new JFrame("Cajero");
 
     public Cajero() {
-        /*
         Connection cn;
         ConxBD con = new ConxBD();
         try {
             cn = con.estbConexion();
             Statement consultaPro = cn.createStatement();
-            ResultSet resultadoPro = consultaPro.executeQuery("Select * from anioD");
+            ResultSet resultadoPro = consultaPro.executeQuery("Select * from productos");
             while (resultadoPro.next()) {
                 comboBox1.addItem(resultadoPro.getString(1));
             }
@@ -37,17 +38,6 @@ public class Cajero extends ventanas{
         } catch (HeadlessException | SQLException f) {
             System.err.println(f);
         }
-        */
-
-        buttonRegistrar.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //Abrir la ventana de ventas
-                Venta ventLogin = new Venta();
-                ventLogin.abrirVentana();
-                addcajero.setVisible(false);
-            }
-        });
         CERRARButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -56,19 +46,90 @@ public class Cajero extends ventanas{
                 addcajero.setVisible(false);
             }
         });
+        comboBox1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Connection cx;
+                ConxBD con = new ConxBD();
+                try{
+                    cx = con.estbConexion();
+                    String qr = "select * from productos  where codProd = "+ comboBox1.getSelectedItem()+";";
+                    Statement s = cx.createStatement();
+                    ResultSet rs = s.executeQuery(qr);
+                    System.out.println(rs);
+                    while(rs.next()) {
+                        JTgetPrecio.setText(rs.getString(2));
+                        JTgetPrecio.setText(rs.getString(3));
+                        JTgetUnidad.setText(rs.getString(4));
+                    }
+                    cx.close();
+                } catch (Exception ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+        BUSCARButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Connection cx;
+                ConxBD con = new ConxBD();
+                try{
+                    cx = con.estbConexion();
+                    String qr = "select * from productos  where codProd = "+ comboBox1.getSelectedItem()+";";
+                    Statement s = cx.createStatement();
+                    ResultSet rs = s.executeQuery(qr);
+                    System.out.println(rs);
+                    while(rs.next()) {
+                        JLnombreProd.setText(rs.getString(2));
+                        JTgetUnidad.setText(rs.getString(3));
+                        JTgetPrecio.setText(rs.getString(4));
+                    }
+                } catch (Exception ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
         buttonRegistrar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                /*
                 Connection cn;
                 ConxBD con = new ConxBD();
+                int codFact = 0, numFact = 0 ;
+                try{
+                    cn = con.estbConexion();
+                    String qr = "select max(codDet) from detalle_factura;";
+                    Statement s = cn.createStatement();
+                    ResultSet rs = s.executeQuery(qr);
+                    System.out.println(rs);
+                    while(rs.next()) {
+                        codFact = Integer.parseInt(rs.getString(1));
+                    }
+                    codFact += 1;
+                } catch (Exception ex) {
+                    throw new RuntimeException(ex);
+                }
+                try{
+                    cn = con.estbConexion();
+                    String qr = "select max(numFacFK) from detalle_factura;";
+                    Statement s = cn.createStatement();
+                    ResultSet rs = s.executeQuery(qr);
+                    System.out.println(rs);
+                    while(rs.next()) {
+                        numFact = Integer.parseInt(rs.getString(1));
+                    }
+                    numFact += 1;
+                } catch (Exception ex) {
+                    throw new RuntimeException(ex);
+                }
                 try {
                     cn = con.estbConexion();
-                    pd = cn.prepareStatement("Insert into datosVehiculo VALUES (?,?,?,?)");
-                    pd.setString(1, comboBox1.getSelectedItem().toString());
-                    pd.setString(2, JTGetDescripoPro.getText());
-                    pd.setString(3, JTgetPrecio.getText());
-                    pd.setString(4, TFsetUnidades.getText());
+                    pd = cn.prepareStatement("Insert into detalle_factura VALUES (?,?,?,?,?,?)");
+                    pd.setString(1, String.valueOf(codFact));
+                    pd.setString(2, String.valueOf(numFact));
+                    pd.setString(3, comboBox1.getSelectedItem().toString());
+                    pd.setString(4, JLnombreProd.getText());
+                    pd.setString(5, TFsetUnidades.getText());
+                    pd.setString(6, JTgetPrecio.getText());
                     System.out.println(pd);
                     int res = pd.executeUpdate();
                     if (res > 0) {
@@ -80,31 +141,10 @@ public class Cajero extends ventanas{
                 } catch (Exception ex) {
                     throw new RuntimeException(ex);
                 }
-                */
-            }
-        });
-        comboBox1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                /*
-                Connection cx;
-                ConxBD con = new ConxBD();
-                try{
-                    cx = con.estbConexion();
-                    String qr = "select * from productos  where codigo = "+ comboBox1.getSelectedItem()+";";
-                    Statement s = cx.createStatement();
-                    ResultSet rs = s.executeQuery(qr);
-                    System.out.println(rs);
-                    while(rs.next()) {
-                        JLdescripcionProd.setText(rs.getString(2));
-                        JTgetPrecio.setText(rs.getString(3));
-                        JTgetUnidad.setText(rs.getString(4));
-                    }
-                    cx.close();
-                } catch (Exception ex) {
-                    throw new RuntimeException(ex);
-                }
-                */
+                //Abrir la ventana de ventas
+                Venta ventLogin = new Venta();
+                ventLogin.abrirVentana();
+                addcajero.setVisible(false);
             }
         });
     }
--- src/Venta.java
+++ src/Venta.java
@@ -15,7 +15,15 @@ public class Venta extends ventanas {
     private JButton REGISTRARButton;
     private JTextField TFgetCodElim;
     private JLabel JLCodigoElim;
+    private JLabel JLgetSub;
+    private JLabel JLgetIva;
+    private JLabel JLgetTotal;
+    private JLabel JLgetNumFact;
+    private JTable table2;
     static JFrame addproduct =  new JFrame("Venta");
+
+    double subW = 1;
+
     PreparedStatement pd;
 
     public Venta() {
@@ -39,44 +47,56 @@ public class Venta extends ventanas {
             @Override
             public void componentResized(ComponentEvent e) {
                 super.componentResized(e);
-                /*
+                double datos1[] = new double[30];
                 Connection cn;
                 ConxBD con = new ConxBD();
                 cn = con.estbConexion();
-                String qr  = "select * from productos";
+                String qr  = "select * from detalle_factura";
                 Statement st;
                 DefaultTableModel model = new DefaultTableModel();
-                model.addColumn("codigo");
-                model.addColumn("nombre");
-                model.addColumn("precio");
+                model.addColumn("codDet");
+                model.addColumn("numFacFK");
+                model.addColumn("codProdFK");
+                model.addColumn("nomProd");
+                model.addColumn("cantProd");
+                model.addColumn("preProd");
                 table1.setModel(model);
-                String [] datos = new String[5];
+                String [] datos = new String[10];
                 try {
                     st = cn.createStatement();
                     ResultSet rs =  st.executeQuery(qr);
+                    int j = 0;
                     while(rs.next()){
                         datos[0] = rs.getString(1);
                         datos[1] = rs.getString(2);
                         datos[2] = rs.getString(3);
+                        datos[3] = rs.getString(4);
+                        datos[4] = rs.getString(5);
+                        datos[5] = rs.getString(6);
+                        datos1[j] = Double.parseDouble(String.valueOf((datos[4])));
+                        j++;
+                        datos1[j] = Double.parseDouble(String.valueOf((datos[5])));
+                        j++;
                         model.addRow(datos);
+                    }
+                    int i = 0;
+                    while(i < j){
+                        subW = datos1[i] * subW;
+                        i++;
                     }
                 } catch (SQLException ex) {
                     throw new RuntimeException(ex);
                 }
-
-                */
-
             }
         });
         ELIMINARButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                /*
                 Connection cn;
                 ConxBD con = new ConxBD();
                 try {
                     cn = con.estbConexion();
-                    pd = cn.prepareStatement("DELETE FROM datosVehiculo WHERE matricula="+ TFgetCodElim);
+                    pd = cn.prepareStatement("DELETE FROM detalle_factura WHERE codProdFK="+ TFgetCodElim);
                     pd.executeUpdate();
                     int res = pd.executeUpdate();
                     if (res > 0) {
@@ -88,7 +108,70 @@ public class Venta extends ventanas {
                 } catch (Exception ex) {
                     throw new RuntimeException(ex);
                 }
-                */
+            }
+        });
+
+        REGISTRARButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Connection cn;
+                ConxBD con = new ConxBD();
+                double ivaW;
+                double totW = 0;
+                try {
+                    cn = con.estbConexion();
+                    String cod = "1121", codDet = "1220";
+                    pd = cn.prepareStatement("Insert into total_factura VALUES (?,?,?,?,?)");
+                    pd.setString(1, cod);
+                    pd.setString(2, codDet);
+                    pd.setString(3, String.valueOf(subW));
+                    pd.setString(4, String.valueOf(ivaW = (subW * 0.12)));
+                    pd.setString(5, String.valueOf(totW = ivaW + subW));
+                    System.out.println(pd);
+                    int res = pd.executeUpdate();
+                    if (res > 0) {
+                        JOptionPane.showMessageDialog(null,"Datos guardados con exito");
+                    } else {
+                        JOptionPane.showMessageDialog(null,"Error");
+                    }
+                    try{
+                        cn = con.estbConexion();
+                        String qr = "select * from total_factura  where codTot = "+ cod+";";
+                        Statement s = cn.createStatement();
+                        ResultSet rs = s.executeQuery(qr);
+                        System.out.println(rs);
+                        while(rs.next()) {
+                            JLgetSub.setText(rs.getString(3));
+                            JLgetIva.setText(rs.getString(4));
+                            JLgetTotal.setText(rs.getString(5));
+                        }
+                    } catch (Exception ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    cn.close();
+                } catch (Exception ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+        JLgetNumFact.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                super.componentResized(e);
+                Connection cx;
+                ConxBD con = new ConxBD();
+                try{
+                    cx = con.estbConexion();
+                    String qr = "select * from total_factura ;";
+                    Statement s = cx.createStatement();
+                    ResultSet rs = s.executeQuery(qr);
+                    System.out.println(rs);
+                    while(rs.next()) {
+                        JLgetNumFact.setText(rs.getString(1));
+                    }
+                } catch (Exception ex) {
+                    throw new RuntimeException(ex);
+                }
             }
         });
     }
